723f1d532ea94344e459e3f1d66718691002d255
juaneins <ballesterosjg@gmail.com>
2019-05-18 22:19:26 -0500

    agregando firebase a environment de producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDnmpTskySP4P3Y0M16-dS_TlLI9i6YM5M",
+    authDomain: "platzinotas-81a9a.firebaseapp.com",
+    databaseURL: "https://platzinotas-81a9a.firebaseio.com",
+    projectId: "platzinotas-81a9a",
+    storageBucket: "platzinotas-81a9a.appspot.com",
+    messagingSenderId: "923284455412",
+    appId: "1:923284455412:web:c9f74b73a668468b"
+  }
 };
